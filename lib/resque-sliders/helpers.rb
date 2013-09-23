4e3b6c002b0549e35566eddf8067cc685ec6c595
David Weiser <davidann@moz.com>
2013-09-23 11:44:49 -0700

    Don't use redis `keys`.

--- lib/resque-sliders/helpers.rb
+++ lib/resque-sliders/helpers.rb
@@ -3,6 +3,10 @@ module Resque
     module ResqueSliders
       module Helpers
 
+        def known_hosts_set_name
+          "plugins:resque-sliders:known_hosts"
+        end
+
         def key_prefix
           "plugins:resque-sliders"
         end
